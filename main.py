eec981893634437aa9cabf8a1b23ba4a2f8106ad
szacharias <matthewsah@hotmail.com>
2018-04-16 02:55:41 +0800

    updated background functions

--- main.py
+++ main.py
@@ -3,86 +3,50 @@ from pygame.locals import *
 import numpy as np
 import time as ti
 # 變數宣告
+##背景顏色
 BACKGROUNDCOLOR = (255, 255, 255)
 BLUE = (0, 0, 255)
 BLACK = (0, 0, 0)
 FPS = 40
 
-VHNUMS = 10
-CELLNUMS = VHNUMS*VHNUMS
+VHNUMS = 10 #格數
+CELLNUMS = VHNUMS*VHNUMS#總格數
+
 
 #  退出
 def terminate():
     pygame.quit()
     sys.exit()
 
-# 判斷是否完成
-def isFinished(board, blackCell):
-    for i in range(CELLNUMS-1):
-        if board[i] != i:
-            return False
-    return True
-
-# 初始化
-pygame.init()
-mainClock = pygame.time.Clock()
-
-# 加載圖片
-gameImage = pygame.image.load('apple.jpg')
-gameImage = pygame.transform.scale(gameImage, (800, 600))
-gameRect = gameImage.get_rect()
+#初始化板子
+def initBoard():
+    pygame.init()
+    mainClock = pygame.time.Clock()
 
 
-# 設置遊戲視窗
-windowSurface = pygame.display.set_mode((gameRect.width, gameRect.height))
-pygame.display.set_caption('拼圖遊戲')
-
-cellWidth = int(gameRect.width / VHNUMS)
-cellHeight = int(gameRect.height / VHNUMS)
-
-finish = False
-
-gameBoard, blackCell = newGameBoard()
-print(gameBoard)
-print(blackCell)
-
-# 遊戲主程式
-while True:
-
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            terminate()
-        if event.type == MOUSEBUTTONDOWN and event.button == 1:
-            x, y = pygame.mouse.get_pos()
-            col = int(x / cellWidth)
-            row = int(y / cellHeight)
-            index = col + row*VHNUMS
-            if (index == blackCell-1 or index == blackCell+1 or index == blackCell-VHNUMS or index == blackCell+VHNUMS):
-                gameBoard[blackCell], gameBoard[index] = gameBoard[index], gameBoard[blackCell]
-                blackCell = index
-                print(gameBoard)
-                print(blackCell)
-
-    if (isFinished(gameBoard, blackCell)):
-        gameBoard[blackCell] = CELLNUMS-1
-        finish = True
-        print ("You win the game!!!")
+    # 加載圖片
+    gameImage = pygame.image.load('apple.jpg')
+    gameImage = pygame.transform.scale(gameImage, (800, 600))
+    gameRect = gameImage.get_rect()
 
 
-    windowSurface.fill(BLACK)
+    # 設置遊戲視窗
+    windowSurface = pygame.display.set_mode((gameRect.width, gameRect.height))
+    pygame.display.set_caption('炸彈超人')
 
-    for i in range(CELLNUMS):
-        if gameBoard[i] == -1:
-            continue
+    #每隔寬度
+    cellWidth = int(gameRect.width / VHNUMS)
+    cellHeight = int(gameRect.height / VHNUMS)
+    hasBoard = True
 
-        rowDst = int(i / VHNUMS)
-        colDst = int(i % VHNUMS)
-        rectDst = pygame.Rect(colDst*cellWidth, rowDst*cellHeight, cellWidth, cellHeight)
+    #畫分隔線
+    for i in range(VHNUMS+1):
+        pygame.draw.line(windowSurface, BLACK, (i*cellWidth, 0), (i*cellWidth, gameRect.height))
+    for i in range(VHNUMS+1):
+        pygame.draw.line(windowSurface, BLACK, (0, i*cellHeight), (gameRect.width, i*cellHeight))
 
-        rowArea = int(gameBoard[i] / VHNUMS)
-        colArea = int(gameBoard[i] % VHNUMS)
-        rectArea = pygame.Rect(colArea*cellWidth, rowArea*cellHeight, cellWidth, cellHeight)
-        windowSurface.blit(gameImage, rectDst, rectArea)
+
+    windowSurface.fill(BLUE)
 
     for i in range(VHNUMS+1):
         pygame.draw.line(windowSurface, BLACK, (i*cellWidth, 0), (i*cellWidth, gameRect.height))
@@ -90,4 +54,98 @@ while True:
         pygame.draw.line(windowSurface, BLACK, (0, i*cellHeight), (gameRect.width, i*cellHeight))
 
     pygame.display.update()
+
+
+#設定新腳色
+class piece:
+    x = 0 #位址
+    y = 0 #位址
+    player = 0 # 1為第一個玩家 2為第二個玩家
+    life = True #存活
+    picture = ""
+
+    def __init__(self , x_position , y_position , player_side , life_TF ):
+        self.x = x_position
+        self.y = y_position
+        self.player = player_side
+        self.life = life_TF
+
+
+#產生腳色
+def init_players():
+    #player1
+    piece( a , 0 , 0 , A , True)
+
+    #player2
+    piece(b , 720 , 520 , B , True)
+
+class obstacles
+    passable = False
+    locationx = -1
+    locationy = -1
+    AOE = -1
+    owner = -1
+    time = -1
+    picked = False
+
+    def initBomb(self , locationx , locationy , AOE , owner , time ):
+        self.locationx = locationx
+        self.locationy = locationy
+        self.AOE = AOE
+        self.owner = owner
+        self.time = time
+        bombTimer = pygame.time.get_ticks()
+        waiting = True
+        while waiting:
+            seconds=(pygame.time.get_ticks()-start_ticks)/1000 #calculate how many seconds
+            if seconds > 5 :
+                bombAOE(self.owner , self.locationx , self.locationy , self.AOE)
+                waiting = False
+
+
+    def initBlocks( self , locationx , locationy ):
+        self.locationx = locationx
+        self.locationy = locationy
+
+    def initItems(self , picked , locationx , locationy ):
+        self.picked = False
+        self.locationx = locationx
+        self.locationy = locationy
+
+def bombAOE(owner , locationx , locationy , AOE):
+
+
+
+finish = False
+# 遊戲主程式
+while finish == False:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w: #player 1 up down left right
+                #x_change = -5
+            if event.key == pygame.K_a:
+                #x_change = 5
+            if event.key == pygame.K_s:
+                #x_change = 5
+            if event.key == pygame.K_d:
+                #x_change = 5
+            if event.key == pygame.K_i: #player 2 up down left right
+                #x_change = -5
+            if event.key == pygame.K_j:
+                #x_change = 5
+            if event.key == pygame.K_k:
+                #x_change = 5
+            if event.key == pygame.K_l:
+                #x_change = 5
+            if event.key == pygame.K_x:#player 1 drop bomb
+                #x_change = 5
+            if event.key == pygame.K_m:#player 2 drop bomb
+                #x_change = 5
+            #這邊我不知道怎麼做
+        '''if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                x_change = 0
+'''
     mainClock.tick(FPS)
+
+    ti.sleep(2)
